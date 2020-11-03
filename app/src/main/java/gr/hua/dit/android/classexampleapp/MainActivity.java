6ecacdbf4624341ff72154bcae5cbe35db8dffc3
Konstantinos Tserpes <tserpes@hua.gr>
2020-11-03 09:47:23 +0200

    Initial commit

---
+++ app/src/main/java/gr/hua/dit/android/classexampleapp/MainActivity.java
@@ -0,0 +1,37 @@
+package gr.hua.dit.android.classexampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.edittext);
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Blah blah");
+            }
+        });
+    }
+}
